0ff77d7e27c91716571e8883c1b2bbd56ee7a2ce
erebus0 <chandraprakashyadav543@gmail.com>
2020-12-12 20:54:06 +0530

    added rest controller

--- src/main/java/com/portfolio/controller/ProjectController.java
+++ src/main/java/com/portfolio/controller/ProjectController.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.portfolio.beans.Project;
 import com.portfolio.service.ProjectService;
-
+@RestController
 public class ProjectController {
 
 	@Autowired
