52eebc93f15bc47be1541b6d61617589cead475c
erebus0 <chandraprakashyadav543@gmail.com>
2020-12-25 16:33:47 +0530

    Deleted Files

--- src/main/java/com/portfolio/service/UserService.java
+++ src/main/java/com/portfolio/service/UserService.java
@@ -1,19 +1,20 @@
 package com.portfolio.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.portfolio.beans.User;
 import com.portfolio.repository.UserRepository;
-
-import java.util.List;
-
 @Service
 public class UserService {
+	
 	@Autowired
 	private UserRepository _userRepository;
 	
 	public List<User> getAll() {
 		return _userRepository.getAll();
-	} 
+	}
+
 }
